4190b80eb02f6882670ebe69538a04884b5099ac
root <root@xrootd-ceph.etc.grid.kiae.ru>
2024-06-11 09:52:00 +0000

    feat: modified nullfs
    Allow statvfs, keep track of the last N created files, always fail
    read and write requests.

--- nul1fs.c
+++ nul1fs.c
@@ -9,14 +9,30 @@
 #define _FILE_OFFSET_BITS 64
 #define FUSE_USE_VERSION 26
 #include <sys/stat.h>
+#include <sys/statvfs.h>
 #include <time.h>
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-time_t start_t;
+#define MAX_MIRROR_LENGTH 1000
+#define MAX_PATH_LENGTH 10000
+
+#define MAX_DIRS 1024
+#define MAX_FILES 1024
+#define MAX_FILENAME_LENGTH 1024
+const char* MOUNT = "/mnt";
+
+char FILES[MAX_FILES][MAX_FILENAME_LENGTH];
+char DIRS[MAX_DIRS][MAX_FILENAME_LENGTH];
+unsigned int CUR_DIR_IDX = 0;
+unsigned int CUR_FILE_IDX = 0;
+time_t start_t = 0;
+
+int tmpi = 0;
 
 static int strendswith(const char *str, const char *sfx) {
     size_t sfx_len = strlen(sfx);
@@ -26,33 +42,50 @@ static int strendswith(const char *str, const char *sfx) {
 };
 
 static int nullfs_isdir(const char *path) {
-    if (! path) return 0;
-    return (strendswith(path, "/") || strendswith(path, "/..")
-        || strendswith(path, "/.") || (strcmp(path, "..") == 0)
-        || (strcmp(path, ".") == 0));
+    int res = 0;
+    int i;
+
+    for (i=0; i<MAX_DIRS; i++) {
+        if (strcmp(path, DIRS[i]) == 0) {
+            res = 1; 
+            break;
+        }
+    }
+    return res;
 };
 
-static int nullfs_getattr(const char *path, struct stat *stbuf) {
+static int nullfs_isfile(const char *path) {
     int res = 0;
-    if (! path) return -ENOENT;
+    int i;
 
-    memset(stbuf, 0, sizeof(struct stat));
-    if (nullfs_isdir(path)) {
+    for (i=0; i<MAX_FILES; i++) {
+        if (strcmp(path, FILES[i]) == 0) {
+            res = 1; 
+            break;
+        }
+    }
+    return res;
+};
+
+
+static int nullfs_getattr(const char *path, struct stat *stbuf) {
+    int i;
+    if (nullfs_isdir(path) || strncmp(path, "/", MAX_FILENAME_LENGTH) == 0) {
         stbuf->st_mode = S_IFDIR | 0777;
         stbuf->st_nlink = 2;
         stbuf->st_atime = time(NULL);
         stbuf->st_mtime = start_t;
         stbuf->st_ctime = start_t;
-    } else {
+    } else if (nullfs_isfile(path)) {
         stbuf->st_mode = S_IFREG | 0666;
         stbuf->st_nlink = 1;
-        stbuf->st_size = 0;
         stbuf->st_atime = time(NULL);
-        stbuf->st_mtime = time(NULL);
-        stbuf->st_ctime = time(NULL);
-    };
-
-    return res;
+        stbuf->st_mtime = start_t;
+        stbuf->st_ctime = start_t;
+    } else {
+        return -ENOENT;
+    }
+    return 0;
 };
 
 static int nullfs_readdir(const char *path, void *buf, fuse_fill_dir_t
@@ -61,6 +94,7 @@ filler, off_t offset, struct fuse_file_info *fi) {
     (void) fi;
 
     if (! nullfs_isdir(path)) return -ENOENT;
+
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
@@ -71,9 +105,12 @@ filler, off_t offset, struct fuse_file_info *fi) {
 static int nullfs_open(const char *path, struct fuse_file_info *fi) {
     (void) fi;
 
-    if (nullfs_isdir(path)) return -ENOENT;
-
+    //if (fi->flags & O_RDONLY) { 
+    //  return -ENOENT;
+    //} else {
     return 0;
+    //}
+
 };
 
 static int nullfs_read(const char *path, char *buf, size_t size,
@@ -83,9 +120,9 @@ off_t offset, struct fuse_file_info *fi) {
     (void) offset;
     (void) fi;
 
-    if (nullfs_isdir(path)) return -ENOENT;
+    /*if (nullfs_isdir(path)) return -ENOENT;*/
 
-    return 0;
+    return -1;
 };
 
 static int nullfs_write(const char *path, const char *buf, size_t size,
@@ -94,9 +131,10 @@ off_t offset, struct fuse_file_info *fi) {
     (void) offset;
     (void) fi;
 
-    if (nullfs_isdir(path)) return -ENOENT;
+    /*if (nullfs_isdir(path)) return -ENOENT;*/
 
-    return (int) size;
+    return -ENOENT;
+    /*return (int) size;*/
 };
 
 static int nullfs_create(const char *path, mode_t m,
@@ -104,8 +142,14 @@ struct fuse_file_info *fi) {
     (void) path;
     (void) m;
     (void) fi;
-
-    return 0;
+    if (nullfs_isfile(path)) {
+        return -EEXIST;
+    } else {
+       CUR_FILE_IDX = CUR_FILE_IDX % MAX_FILES;
+       strncpy(FILES[CUR_FILE_IDX], path, MAX_FILENAME_LENGTH-1 );
+       CUR_FILE_IDX += 1;
+       return 0;
+    }
 };
 
 static int nullfs_unlink(const char *path) {
@@ -150,6 +194,32 @@ static int nullfs_utimens(const char *path, const struct timespec ts[2]) {
     return 0;
 };
 
+static int nullfs_statfs(const char *path, struct statvfs* vfs) {
+    (void) path;
+    vfs->f_bsize = 1024;
+    vfs->f_frsize = 0;
+    vfs->f_blocks = 1024*1024*1024;
+    vfs->f_bfree = 1024*1024*1024;
+    vfs->f_bavail = 1024*1024*1024;
+    vfs->f_files = 0;
+    vfs->f_ffree = 1024*1024*1024;
+    vfs->f_favail = 1024*1024*1024;
+
+    return 0;
+};
+
+static int nullfs_mkdir(const char *path, mode_t mode) {
+    if (nullfs_isdir(path)) {
+        return -EEXIST;
+    } else {
+        CUR_DIR_IDX = CUR_DIR_IDX % MAX_DIRS;
+        strncpy(DIRS[CUR_DIR_IDX], path, MAX_FILENAME_LENGTH-1 );
+        CUR_DIR_IDX += 1;
+        return 0;
+    }
+};
+
+
 static struct fuse_operations nullfs_oper = {
     .getattr    = nullfs_getattr,
     .readdir    = nullfs_readdir,
@@ -164,6 +234,8 @@ static struct fuse_operations nullfs_oper = {
     .chmod      = nullfs_chmod,
     .chown      = nullfs_chown,
     .utimens    = nullfs_utimens,
+    .statfs     = nullfs_statfs,
+    .mkdir      = nullfs_mkdir,
 };
 
 int main(int argc, char *argv[]) {
